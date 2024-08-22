f5b48a436adafb2d65065a65e983c7c2b3e578a8
Shorno <roy15-6141@s.diu.edu.bd>
2024-08-22 22:11:37 +0600

    styles via CDN

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily : {
-        "manrope" : ["Manrope", "sans-serif"]
-      }
+    content: [
+        "./index.html",
+        "./src/**/*.{html,js}"
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                "manrope": ["Manrope", "sans-serif"]
+            }
+        },
     },
-  },
-  plugins: [require("daisyui")],
+    plugins: [require("daisyui")],
 }
 
